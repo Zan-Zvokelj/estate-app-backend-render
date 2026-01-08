fa62fc6b6133004071666d781f90c9f9173dc2f7
zanzvokelj <zanzvokelj@gmail.com>
2026-01-08 12:40:39 +0100

    Fix WSGI and DJANGO_SETTINGS_MODULE paths

--- ESTATE_APP_BACKEND/ESTATE_APP_BACKEND/wsgi.py
+++ ESTATE_APP_BACKEND/ESTATE_APP_BACKEND/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESTATE_APP_BACKEND.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'ESTATE_APP_BACKEND.ESTATE_APP_BACKEND.settings'
+)
+
 
 application = get_wsgi_application()
