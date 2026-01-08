427909b65ac59db22c38ce65fdc7b80ef7fc4368
zanzvokelj <zanzvokelj@gmail.com>
2026-01-08 12:58:59 +0100

    Fix ROOT_URLCONF for Render

--- ESTATE_APP_BACKEND/properties/views.py
+++ ESTATE_APP_BACKEND/properties/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from properties.serializer import PropertySerializer
+from ESTATE_APP_BACKEND.properties.serializer import PropertySerializer
+
 from rest_framework import views, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
